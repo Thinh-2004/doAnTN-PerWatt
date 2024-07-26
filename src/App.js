04d7770d20bc89792d5e1c87fe878c678c0d0bdb
thinhtqpc05167 <139950912+thinhtqpc05167@users.noreply.github.com>
2024-07-27 01:40:50 +0700

    .

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 
 import './App.css';
-
 import Form from './components/Login&Register/Form';
-
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Home from './components/UI&UX/Body/Home/Home';
+import InfoBody from './components/Order/Body/InfoBody';
 
 function App() {
   return (
    <BrowserRouter>
     <Routes>
-      <Route path='/' element={ <Home></Home>}></Route>
-      <Route path='/login' element={<Form></Form>}></Route>
+      <Route path='' element={ <Home></Home>}></Route>
+      <Route path='login' element={<Form></Form>}></Route>
+      <Route path='/cart' element={<InfoBody></InfoBody>}></Route>
     </Routes>
    </BrowserRouter>
   );
--- src/components/UI&UX/Header/Header.js
+++ src/components/UI&UX/Header/Header.js
@@ -34,6 +34,7 @@ const Header = () => {
             <Link
               type="button"
               class="btn btn-icon position-relative rounded-4"
+              to={"/cart"}
             >
               <i class="bi bi-cart4 fs-4"></i>
               <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
